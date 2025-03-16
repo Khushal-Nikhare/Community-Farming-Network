e438a0a9c59eb5ad3e35a51c43660960b8f864f3
AaryaDubey26 <aaryadubey263@gmail.com>
2025-03-16 16:09:41 +0530

    update categories

--- CommunityFarmingNetwork/CommunityFarmingNetwork/settings.py
+++ CommunityFarmingNetwork/CommunityFarmingNetwork/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3&8vvxx^+bdpby4)nuv_ol=+@52o771oy^p8qa$vziyig#)#3$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
